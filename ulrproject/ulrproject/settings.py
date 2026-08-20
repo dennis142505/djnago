386959f256fee74f2ede561c2ea85f49679863ec
dennis142505 <dennis2004dasdp2@gmail.com>
2026-08-20 14:33:16 +0530

    feat: Update settings and WSGI configuration for deployment

--- ulrproject/ulrproject/settings.py
+++ ulrproject/ulrproject/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    "ulrshorner.vercel.app",
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
--- ulrproject/ulrproject/wsgi.py
+++ ulrproject/ulrproject/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ulrproject.settings')
 
 application = get_wsgi_application()
+
+
